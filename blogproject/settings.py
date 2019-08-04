4aaec0eac887ee3b38d9a4282cd15c12435b359a
Seran <sara100477@gmail.com>
2019-08-04 16:36:01 +0900

    last

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -120,9 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
+os.makedirs(STATIC_TMP, exist_ok=True)
+of.makedirs(STATIC_ROOT, exist_ok=True)
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
